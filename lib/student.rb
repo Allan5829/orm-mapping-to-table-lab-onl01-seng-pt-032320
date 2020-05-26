04a4427adf5e4ea5273cf22db53863acb03b5f65
Allan Farca <allan.farca@gmail.com>
2020-05-26 04:24:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,40 +1,40 @@
 class Student
-   
+
   attr_accessor :name, :grade
   attr_reader :id
- 
+
   def initialize(name, grade, id=nil)
     @id = id
     @name = name
     @grade = grade
   end
- 
+
   def self.create_table
-    sql =  <<-SQL 
+    sql =  <<-SQL
       CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
+        id INTEGER PRIMARY KEY,
+        name TEXT,
         grade TEXT
         )
         SQL
-    DB[:conn].execute(sql) 
+    DB[:conn].execute(sql)
   end
 
   def self.drop_table
     sql = "DROP TABLE students;"
     DB[:conn].execute(sql)
-  end 
- 
+  end
+
   def save
     sql = <<-SQL
-      INSERT INTO songs (name, album) 
+      INSERT INTO songs (name, album)
       VALUES (?, ?)
     SQL
- 
+
     DB[:conn].execute(sql, self.name, self.album)
- 
+
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
- 
+
   end
 
   def self.create(name, album)
@@ -42,5 +42,5 @@ class Student
     song.save
     song
   end
-  
+
 end
